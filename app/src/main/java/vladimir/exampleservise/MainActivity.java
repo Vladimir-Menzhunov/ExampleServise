41da33ba7fd642b29eabce0749b77123c71d4ae1
vladimir <vi.vova2000@gmail.com>
2020-07-13 18:33:06 +0300

    add two button and service CountService.java also add ScheduledExecutorService shows the number of milliseconds from the start of the program.

--- app/src/main/java/vladimir/exampleservise/MainActivity.java
+++ app/src/main/java/vladimir/exampleservise/MainActivity.java
@@ -2,13 +2,42 @@ package vladimir.exampleservise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import vladimir.exampleservise.services.CountService;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button butStart;
+    Button butStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        butStart = findViewById(R.id.but_start_service);
+        butStop = findViewById(R.id.but_stop_service);
+
+        butStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CountService.class);
+                startService(intent);
+            }
+        });
+
+        butStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CountService.class);
+                stopService(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/vladimir/exampleservise/services/CountService.java
@@ -0,0 +1,58 @@
+package vladimir.exampleservise.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class CountService extends Service {
+
+    private final String TAG = this.getClass().getName();
+    private ScheduledExecutorService executorService;
+
+    public CountService() {
+
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate:");
+        executorService = Executors.newSingleThreadScheduledExecutor();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand:");
+
+        executorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                Log.d(TAG, "currentSecond: " + System.currentTimeMillis() * 1000);
+            }
+        },0, 1, TimeUnit.SECONDS);
+
+        return START_STICKY; // Если сервис будет прерван устройством, то он будет ждать когда появится возможность востановить сервис
+
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        executorService.shutdownNow();
+        Log.d(TAG, "onDestroy:");
+    }
+}
